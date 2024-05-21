c0dd39edfa9d034617062363d949452216eb8a45
eduardo01322 <eduardo.errea32@gmail.com>
2024-05-21 15:28:43 -0300

    .

--- src/components/FooterAdm.tsx
+++ src/components/FooterAdm.tsx
@@ -1,19 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 function FooterAdm(): React.ReactElement {
-  
+    const [elementVisible, setElementVisible] = useState(false);
     const navigation = useNavigation(); 
 
     return (
       <View style={styles.footer}>
-      <TouchableOpacity onPress={()=> navigation.navigate('Cadastro')}>
-          <Image source={require('../assets/images/home.png')} 
-          style={styles.footerIcon}/>
-      </TouchableOpacity>
         <TouchableOpacity onPress={ () => navigation.navigate('listagem')}>
-        <Image source={require('../assets/images/addUser.png')} 
+        <Image source={require('../assets/images/home.png')} 
           style={styles.footerIcon}/>
         </TouchableOpacity>
         <TouchableOpacity>
@@ -24,7 +20,25 @@ function FooterAdm(): React.ReactElement {
             <Image source={require('../assets/images/config.png')} 
             style={styles.footerIcon}/>
         </TouchableOpacity>
-        <TouchableOpacity>
+        
+        {elementVisible ? ( 
+        <View style={styles.cadastrosFundo}>
+        <TouchableOpacity onPress={()=> navigation.navigate('Cadastro')}
+        style={styles.cadastros}>
+            <Text style={styles.cadastrosText}>Cadastro filmes</Text>
+        </TouchableOpacity> 
+
+        <TouchableOpacity style={styles.cadastros}>
+        <Text style={styles.cadastrosText}>Cadastro animação</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.cadastros}> 
+            <Text style={styles.cadastrosText}>Cadastro serie</Text>
+        </TouchableOpacity>
+        </View>
+        ) : null} 
+
+        <TouchableOpacity onPress={() => setElementVisible(!elementVisible)}>
             <Image source={require('../assets/images/plus.png')} 
             style={styles.footerIcon}/>
         </TouchableOpacity>
@@ -36,7 +50,18 @@ const styles = StyleSheet.create({
     menuList: {
         flexGrow: 1
     },
-
+    cadastros: {
+        marginTop: -50,
+        left: 70,
+    },
+    cadastrosFundo: {
+        backgroundColor: 'white'
+    },
+    cadastrosText: {
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 15,
+    },
     footer: {
         borderTopWidth: 0.2, 
         backgroundColor: 'white', 
